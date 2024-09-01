04feb0f384e85280c61c9f5149bf5e53f342ece2
njs33487 <njs_33487@outlook.com>
2024-09-01 10:53:30 -0400

    DRAFT adding styles to producDetials 202409011053

--- src/components/Products/ProductDetails.tsx
+++ src/components/Products/ProductDetails.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 import { Product } from "../../../types/types";
+import styles from "./productList.module.css";
+
 import { cartService } from "../../../api/services/CartService";
 import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-  const { productId } = useParams()
+  const { productId } = useParams();
   const [product, setProduct] = useState<Product | null>(null);
 
   useEffect(() => {
     const fetchProduct = async () => {
-      const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+      const response = await fetch(
+        `https://fakestoreapi.com/products/${productId}`
+      );
       const data = await response.json();
       setProduct(data);
     };
@@ -19,11 +23,21 @@ const ProductDetails = () => {
 
   return (
     <>
-      <p>{product.description}</p>
-      <p>Price: ${product.price.toFixed(2)}</p>
-      <button onClick={() => cartService.addToCart(product)}>
-        Add to Cart
-      </button>
+      <div key={product.id} className={styles.card}>
+        <img
+          className={styles.cardImage}
+          src={product.image}
+          alt={product.title}
+        />
+        <div className={styles.cardContent}>
+          <h2 className={styles.cardTitle}>{product.title}</h2>
+          <p className={styles.cardPrice}>${product.price.toFixed(2)}</p>
+          <p>{product.description}</p>
+          <button onClick={() => cartService.addToCart(product)}>
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </>
   );
 };
