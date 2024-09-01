a82ba12b88a0fd10c477a745b09be0a94c9ba53d
njs33487 <njs_33487@outlook.com>
2024-09-01 08:07:36 -0400

    DRAFT: update productDetails

--- src/components/Products/ProductDetails.tsx
+++ src/components/Products/ProductDetails.tsx
@@ -1,29 +1,31 @@
 import { useEffect, useState } from "react";
 import { Product } from "../../../types/types";
 import { ProductService } from "../../../api/services/ProductService";
-import { useParams } from "react-router-dom";
-import styles from 'productList.module.css';
+import { cartService } from "../../../api/services/CartService";
 
-export const ProductDetails = () => {
-  const { id } = useParams();
+interface ProductDetailProps {
+  productId: number;
+}
+
+export const ProductDetails = ({ productId }: ProductDetailProps) => {
   const [product, setProduct] = useState<Product | null>(null);
 
   useEffect(() => {
-    ProductService.getProductById(id)
-      .then((response) => setProduct(response.data))
-      .catch((error) => console.error("Error fetching product details", error));
-  }, [id]);
+    const fetchProduct = async () => {
+      const data = await ProductService.getProductById(productId);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
   if (!product) return <div>Loading...</div>;
 
   return (
     <>
-      <div className={styles.card}>
-        <h3 className={styles.cardTitle}>{product.title}</h3>
-        <img src={product.image} className={styles.cardImage} alt={product.title} />
-        <p className={styles.cardContent}>{product.description}</p>
-        <p className={styles.cardPrice}>Price: ${product.price.toFixed(2)}</p>
-        <button className={styles.cardButton}>Add to Cart</button>
-      </div>
+      <p>{product.description}</p>
+      <p>Price: ${product.price.toFixed(2)}</p>
+      <button onClick={() => cartService.addToCart(product)}>
+        Add to Cart
+      </button>
     </>
   );
 };
